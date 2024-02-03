4f40162ba26eba5eadebb289db5f3fe6999d368c
Konstantinos Athanasiou <dinosath0@gmail.com>
2024-02-03 15:59:29 +0200

    initial commit

---
+++ migration/src/m20240203_115859_templates.rs
@@ -0,0 +1,37 @@
+use std::borrow::BorrowMut;
+
+use loco_rs::schema::*;
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                table_auto(Templates::Table)
+                    .col(pk_auto(Templates::Id).borrow_mut())
+                    .col(text_null(Templates::Content).borrow_mut())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Templates::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Templates {
+    Table,
+    Id,
+    Content,
+    
+}
+
+
